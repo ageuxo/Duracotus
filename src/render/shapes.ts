8bfc66e01f85308945595b314e4493f96444e142
ageuxo <ageuxo@gmail.com>
2026-03-30 11:29:09 +0200

    Add createNGons function to generate vertices of a regular polygon with n sides

--- src/render/shapes.ts
+++ src/render/shapes.ts
@@ -27,3 +27,44 @@ export function createTriangle([ x, y, z ]: vec3) {
 
   return vertices;
 }
+
+export function createNGon([ x, y, z ]: vec3, n: number, radius: number) {
+  const points: vec3[] = [];
+
+  for (let i = 0; i < n; i++) {
+    const point: vec3 = [
+      (x + radius * Math.cos(i * 2 * Math.PI / n)),
+      (y + radius * Math.sin(i * 2 * Math.PI / n)),
+      z
+    ];
+    points.push(point);
+  }
+
+  const vertices: number[] = [];
+
+  function pushVert([ x, y, z ]: vec3) {
+    vertices.push(x);
+    vertices.push(y);
+    vertices.push(z);
+  }
+
+  function fromIdx(idx: number) {
+    if (idx < 0) {
+      idx = idx + points.length;
+    }
+    return points[idx];
+  }
+
+  // Make a triangle fan from the center of the circle
+  for (let idx = 0; idx < points.length; idx++) {
+    const v0: vec3 = fromIdx(idx - 1);
+    const v1: vec3 = [x, y, z];
+    const v2: vec3 = fromIdx(idx);
+
+    pushVert(v0);
+    pushVert(v1);
+    pushVert(v2);
+  }
+
+  return vertices;
+}
