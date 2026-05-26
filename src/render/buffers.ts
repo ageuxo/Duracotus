b590a1f671324c161ebbf7506f63c1a3203d6fd7
ageuxo <ageuxo@gmail.com>
2026-05-26 15:50:48 +0200

    Clean up unused in buffers.ts

--- src/render/buffers.ts
+++ src/render/buffers.ts
@@ -1,18 +1,7 @@
-import { GLContext } from "./render";
 
 export type Buffers = {
   [key: string]: WebGLBuffer;
 };
-
-export function createBuffers({ gl }: GLContext) {
-  const buffers: Buffers = {
-    position: gl.createBuffer(),
-    indices: gl.createBuffer(),
-    colour: gl.createBuffer()
-  }
-
-  return buffers;
-}
 
 export function uploadFloatBuffer(gl: WebGL2RenderingContext, buf: WebGLBuffer, data: number[]) {
   gl.bindBuffer(gl.ARRAY_BUFFER, buf);
